86a0cf58ad0f8a9e4558b78b6305e428bbfe58c3
Mark A <emkay1993@gmail.com>
2021-02-19 09:15:05 +0000

    Fix loop and win conditions

--- bin/main.rb
+++ bin/main.rb
@@ -19,19 +19,17 @@ end
 
 def results_simple1(board)
   case 4.even?
-  when board[0...3].all?('X') then puts 'Winning Move! Game has been won by X at the top'
-  when board[3...6].all?('X') then puts 'Winning Move! Game has been won by X in the middle'
-  when board[6...9].all?('X') then puts 'Winning Move! Game has been won by X at the bottom'
-  else 'No wins'
+  when board[0...3].all?('X') then puts 'Winning Move! Game has been won by X at the top', @win_condition = ' '
+  when board[3...6].all?('X') then puts 'Winning Move! Game has been won by X in the middle', @win_condition = ' '
+  when board[6...9].all?('X') then puts 'Winning Move! Game has been won by X at the bottom', @win_condition = ' '
   end
 end
 
 def results_simple2(board)
   case 4.even?
-  when board[0...3].all?('O') then puts 'Winning Move! Game has been won by O at the top'
-  when board[3...6].all?('O') then puts 'Winning Move! Game has been won by O in the middle'
-  when board[6...9].all?('O') then puts 'Winning Move! Game has been won by O at the bottom'
-  else 'No wins'
+  when board[0...3].all?('O') then puts 'Winning Move! Game has been won by O at the top', @win_condition = ' '
+  when board[3...6].all?('O') then puts 'Winning Move! Game has been won by O in the middle', @win_condition = ' '
+  when board[6...9].all?('O') then puts 'Winning Move! Game has been won by O at the bottom', @win_condition = ' '
   end
 end
 
@@ -39,13 +37,13 @@ def results_x(board)
   case 4.even?
   # board Left to Bottom condition
   when board.values_at(0, 3, 6).all?('X')
-    puts 'Winning Move! Game has been won by X from left to bottom'
+    puts 'Winning Move! Game has been won by X from left to bottom', @win_condition = ' '
   when board.values_at(2, 5, 8).all?('X')
-    puts 'Winning Move! Game has been won by X from right to bottom'
+    puts 'Winning Move! Game has been won by X from right to bottom', @win_condition = ' '
   when board.values_at(0, 4, 8).all?('X')
-    puts 'Winning Move! Game has been won by X diagonally from left row'
+    puts 'Winning Move! Game has been won by X diagonally from left row', @win_condition = ' '
   when board.values_at(2, 4, 6).all?('X')
-    puts 'Winning Move! Game has been won by X diagonally from right row'
+    puts 'Winning Move! Game has been won by X diagonally from right row', @win_condition = ' '
   end
 end
 
@@ -53,22 +51,23 @@ def results_o(board)
   case 4.even?
   # board Left to Bottom condition
   when board.values_at(0, 3, 6).all?('O')
-    puts 'Winning Move! Game has been won by O from left to bottom'
+    puts 'Winning Move! Game has been won by O from left to bottom', @win_condition = ' '
   when board.values_at(2, 5, 8).all?('O')
-    puts 'Winning Move! Game has been won by O from right to bottom'
+    puts 'Winning Move! Game has been won by O from right to bottom', @win_condition = ' '
   when board.values_at(0, 4, 8).all?('O')
-    puts 'Winning Move! Game has been won by O diagonally from left row'
+    puts 'Winning Move! Game has been won by O diagonally from left row', @win_condition = ' '
   when board.values_at(2, 4, 6).all?('O')
-    puts 'Winning Move! Game has been won by O diagonally from right row'
+    puts 'Winning Move! Game has been won by O diagonally from right row', @win_condition = ' '
   end
 end
 
 class User1
   attr_accessor :username
-  attr_writer :password
+  attr_writer :password, :win_condition
 
-  def initialize(username)
+  def initialize(username, win_condition)
     @username = username
+    @win_condition = win_condition
   end
 
   #  String that is shown when variable is called
@@ -89,14 +88,14 @@ board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 
 puts 'Hello Player 1. Please enter your username here:'
 player1 = gets.chomp
-player1 = User1.new(player1)
+player1 = User1.new(player1, false)
 # Store this username in a variable
 puts player1.info
 # Player 1 is now X
 
 puts 'Hello Player 2. Please enter your username here:'
 player2 = gets.chomp
-player2 = User1.new(player2)
+player2 = User1.new(player2, false)
 # Store this username in a variable
 puts player2.info2
 # Player 2 is now O
@@ -111,7 +110,7 @@ puts "#{player1.username} you may go first."
 # Random player is chosen to start the game.
 total_moves = 0
 
-while total_moves < 8
+until total_moves > 9
   # player_moves1
   number = false
   loop do
@@ -126,16 +125,19 @@ while total_moves < 8
       display_board(board)
       puts results_simple1(board)
       puts results_x(board)
-      player1.your_turn
       number = true
       total_moves += 1
       break
     end
   end
 
+  break puts 'The game is a draw!' if board[0..9].any?(' ') == false
+  break if @win_condition == ' '
+
   # player_moves2
   number2 = false
   loop do
+    player2.your_turn
     puts 'Please enter a number from 1-9'
     number2 = gets.chomp.to_i
     if board[number2 - 1] == 'O' || board[number2 - 1] == 'X'
@@ -147,13 +149,11 @@ while total_moves < 8
       display_board(board)
       puts results_simple2(board)
       puts results_o(board)
-      player2.your_turn
+      player1.your_turn
       number2 = true
       total_moves += 1
       break
     end
   end
-
+  break if @win_condition == ' '
 end
-
-puts 'Game is a draw!'
---
+++ bin/test.rb
@@ -0,0 +1,3 @@
+board = ['X', 'X', 'O', 'O', 'X', 'X', 'O', 'X', 'O']
+
+puts board[0..9].any?(' ')
