f1361ad581fb2f81237f732275c6f0f0560e3aaf
Mark A <emkay1993@gmail.com>
2021-02-24 10:53:58 +0000

    Add game testing

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,13 +1,16 @@
-#spec/main_spec.rb
+# spec/main_spec.rb
 require './lib/game_logic'
 require './bin/main'
+
+board = MainBoard.new
+board = board.main_board
+board1 = BoardMoves.new
+boardloop = BoardLoop.new
+boardloop.board_loop(board)
 
 describe BoardMoves do
   describe '.player_moves1' do
     it 'start player 1 loop' do
-      board1 = BoardMoves.new
-      board = MainBoard.new
-      board = board.main_board
       expect(board1.player_moves1(board)).to eql(nil)
     end
   end
@@ -55,10 +58,6 @@ end
 
 describe BoardLoop do
   describe '.player_moves1' do
-    board = MainBoard.new
-    board = board.main_board
-    boardloop = BoardLoop.new
-    boardloop.board_loop(board)
     let(:board_loop) do
     end
     it 'should be the child class of BoardLoop' do
@@ -67,15 +66,33 @@ describe BoardLoop do
   end
 end
 
-describe BoardMoves do
-  describe '.player_moves1' do
-    board = MainBoard.new
-    board = board.main_board
-    board1 = BoardMoves.new
-    let(:player_moves1) do
+describe MainBoard do
+  describe '.main_board' do
+    let(:board) do
     end
-    it 'should be the child class of BoardLoop' do
-      expect(board1.player_moves1(board)).to eql(BoardLoop)
+    it 'should output the game board' do
+      expect(board.class).not_to eql(nil)
+    end
+  end
+end
+
+describe Greetings do
+  describe '.greeting' do
+    greeting = Greetings.new
+    let(:greeting) do
+    end
+    it 'class should be Greetings' do
+      expect(greeting.class).to eql(Greetings)
+    end
+  end
+end
+
+describe BoardResults do
+  describe '.results_simple_x' do
+    let(:BoardResults) do
+    end
+    it 'should output a string' do
+      expect(BoardResults.class).not_to eql(NilClass)
     end
   end
 end
