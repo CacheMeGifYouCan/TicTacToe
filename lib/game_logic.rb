48b10e263cd846b93c41f3b755b16f1428e8190e
Mark A <emkay1993@gmail.com>
2021-03-15 10:36:17 +0000

    Add game comments

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -1,5 +1,17 @@
 #!/usr/lib/env ruby
 
+<<<<<<< Updated upstream
+=======
+# Class that initialises the game board
+#
+# It keeps stores the data pertaining the current state of
+# the board in an array
+#
+# board = MainBoard.new
+# board = board.main_board
+# puts board    # -> # [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+
+>>>>>>> Stashed changes
 class MainBoard
   attr_reader :game_board
 
@@ -8,6 +20,20 @@ class MainBoard
   end
 end
 
+<<<<<<< Updated upstream
+=======
+# The display_board method outputs the board to the console when called,
+# this gives a visual output to the user, displaying the board status
+#
+# It is reliant on data from the main_board method
+#
+# puts display_board(board)
+#
+# X | O | X
+# O | X | O
+# X | O | X
+#
+>>>>>>> Stashed changes
 def display_board(board)
   puts
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
@@ -18,6 +44,18 @@ def display_board(board)
   puts
 end
 
+<<<<<<< Updated upstream
+=======
+# Class stores the player username, password and current win condition.
+# The methods defined have been created to output various interactions,
+# with the user.
+#
+# player1 = gets.chomp
+# puts player1 = User1.new(player1, false)  # -> # 'Example', false
+#
+# puts player1.info  # -> 'Hello Example, you are X'
+#
+>>>>>>> Stashed changes
 class User1
   attr_reader :username
   attr_writer :password, :win_condition
@@ -47,6 +85,16 @@ Hello #{@username}, you are O
   end
 end
 
+# Class uses methods to check the current game progress
+#
+# When methods inside are called, they will output strings,
+# dependant on the various conditions specified.
+#
+# E.g. # -> # board_results = BoardResults.new
+#
+# puts board_results.results_advanced1(board)
+# -> 'Game won diagonally!'
+#
 class BoardResults
   def results_simple_x(board)
     x_check = %w[X X X]
@@ -99,6 +147,17 @@ class BoardResults
   end
 end
 
+# Class uses two methods for both player1 and player2 moves
+#
+# It is constantly providing user interaction, utilising other class methods,
+# and adding new player moves to the board array object
+#
+# E.g. # board1 = BoardMoves.new
+# board1.player_moves1(board)
+# ->
+# 'X, it's your turn. Please enter a number from 1-9'
+# (displays board with players new turn added)
+
 class BoardMoves
   def player_moves1(board)
     board_results = BoardResults.new
@@ -147,6 +206,8 @@ class BoardMoves
   end
 end
 
+# This Class loops through both players turns until a win/draw condition has been met
+#
 class BoardLoop
   def board_loop(board)
     total_moves = 0
